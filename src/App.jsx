ec2755cc822657d85da2a879c31813d6ef193dda
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-12-15 22:35:53 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 class="text-center text-3xl text-blue-900 bg-amber-950 px-8">Hello</h1>
+      <h1 class="text-center text-3xl text-blue-900 bg-amber-950 px-8">Hello !!</h1>
     </>
   )
 }
