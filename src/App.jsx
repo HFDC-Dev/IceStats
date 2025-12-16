a1c7d930ea7d514a504672353a306959b4b725a3
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-12-16 23:38:16 +0100

    Installation de DaisyUI - Ajout de react-router et ajout d'un thème

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <h1 class="text-center text-3xl text-blue-900 bg-amber-950 px-8">Hello !!</h1>
-    </>
+    <Routes>
+
+    </Routes>
   )
 }
 
