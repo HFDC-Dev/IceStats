c193bc8ec79478c119c3d217ba60b43739e2d7f3
HFDC-Dev <henriquedacosta.pro@gmail.com>
2026-01-05 19:50:11 +0100

    Réorganisation du projet : création dossiers frontend et backend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <main className="text-center text-3xl p-18">
+            <h1 className="">Match du jour</h1>
+        </main>
+    )
+}
+
+export default Home
